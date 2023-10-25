e2904996c21caaf8962a2907c47631be37b623b6
Aran <aransjannson@gmail.com>
2023-10-26 00:35:16 +0100

    Initial Commit

---
+++ AranJannson/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+    <header id="header">
+      <h1 style="text-align: center; color:white; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;  padding-top: 20px; background-color: #2d2d2d;">
+        Aran Jannson
+        <hr style="height:2px; color:black; border-width:0; background-color:black;">
+      </h1>
+  </header>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
