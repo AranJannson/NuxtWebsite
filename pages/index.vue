ff1f48d7cdf2bd41bec9555acc5c06cc322774c1
AranJannson <aransjannson@gmail.com>
2023-12-12 12:19:09 +0000

    Changed file path, fixed gramar

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
   <div class="welcomeBox" >
     <h2>Welcome!</h2>
 
-    <p style="color:gainsboro;">Hello My Name Is <b>Aran Jannson</b>. I am a second-year undergraduate student studying Computer Science at the <a href="https://www.surrey.ac.uk/" style="color:antiquewhite;">The University Of Surrey</a>.</p>
+    <p style="color:gainsboro;">Hello My Name Is <b>Aran Jannson</b>. I am a second-year undergraduate student studying Computer Science at <a href="https://www.surrey.ac.uk/" style="color:antiquewhite;">The University Of Surrey</a>.</p>
     <p style="color:gainsboro;">Welcome to my portfolio website! Here, you can explore my most popular GitHub projects, find ways to reach me, and discover more details about myself.</p>
   </div>
 
