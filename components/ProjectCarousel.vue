fb90d74f4b40e9840bae8e4aaf0f876d3fa6d04c
AranJannson <me@aranjannson.com>
2024-02-03 01:47:17 +0000

    Updated image again

--- components/ProjectCarousel.vue
+++ components/ProjectCarousel.vue
@@ -57,7 +57,7 @@ const originalProjects = [
   {
     name: 'projectPortfolioWebsite.title',
     description: 'projectPortfolioWebsite.description',
-    imageUrl: 'https://aranjannson.com/favicon.svg',
+    imageUrl: 'https://aranjannson.com/favicon.png',
     language: 'Nuxt3',
     link: 'https://github.com/AranJannson/PortfolioWebsite'
   },
