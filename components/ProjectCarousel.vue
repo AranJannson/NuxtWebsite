48f9f18405c14c4f0978b2e787b57a8e020151f3
AranJannson <me@aranjannson.com>
2024-02-13 14:33:05 +0000

    Testing GPG

--- components/ProjectCarousel.vue
+++ components/ProjectCarousel.vue
@@ -14,6 +14,7 @@
           <a :href="project.link">
             <img :src="project.imageUrl" alt="Project Image">
           </a>
+
           <div>
             <p>{{ $t('language') }}: </p>
             <p :class="`${project.language.toLowerCase()}-text`">{{ project.language }}</p>
